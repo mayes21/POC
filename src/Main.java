2ba18b04b8abed9e9f911d2cfd2c7fddcc5680ad
Amayas ABBOUTE <amayas.abboute@smile.fr>
2017-04-09 19:43:12 +0200

    optimization + Main class comments

--- src/Main.java
+++ src/Main.java
@@ -18,16 +18,16 @@ public class Main {
             e.printStackTrace();
         }
 
+        /* Init date/time */
         LocalDate today = new LocalDate();
+        /* first day of every last month */
         LocalDate startDate = today.minusMonths(1).dayOfMonth().withMinimumValue();
+        /* first day of current month */
         LocalDate startNextMonth = today.dayOfMonth().withMinimumValue();
+        /* First time of day : MIDNIGHT */
         LocalTime endTime = LocalTime.MIDNIGHT.minusSeconds(1);
 
-
-        System.out.println(startDate);
-        System.out.println(startNextMonth);
-
-
+        /* Date/Time formatter */
         DateTimeFormatter dtfForPrint = DateTimeFormat.forPattern("E d MMM");
         DateTimeFormatter dtfForDB = DateTimeFormat.forPattern("yyyy-MM-dd");
         DateTimeFormatter timePattern = DateTimeFormat.forPattern("HH:mm:ss");
@@ -37,18 +37,18 @@ public class Main {
             LocalTime time = LocalTime.MIDNIGHT;
             do {
                 System.out.println(timePattern.print(time));
-                String query="SELECT uid FROM connexion WHERE (time_con BETWEEN ? AND ?) AND date_con LIKE ?";
-                PreparedStatement preparedStatement = null;
+                String query="SELECT count(*) FROM connexion WHERE (time_con BETWEEN ? AND ?) AND date_con LIKE ?";
+                PreparedStatement preparedStatement;
                 try {
+                    /* prepare query */
                     preparedStatement = DBConnection.connection.prepareStatement(query);
                     preparedStatement.setString(1, time.toString());
                     preparedStatement.setString(2, time.plusHours(1).toString());
                     preparedStatement.setString(3,dtfForDB.print(date));
                     ResultSet rs = preparedStatement.executeQuery();
-                    while (rs.next()) {
-                        int uid = rs.getInt("uid");
-                        System.out.println(uid);
-                    }
+                    /* display result */
+                    rs.next();
+                    System.out.println(rs.getInt(1));
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
